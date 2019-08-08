b00d9087dbeb0c794da016eb7478aff1bfd15fdd
Jfleming <Jfleming07712@gmail.com>
2019-08-08 19:17:41 -0400

    Save/Load fully implemented with verification and try/catch for exceptions.

--- TravelingExperiment/LoadGame.cs
+++ TravelingExperiment/LoadGame.cs
@@ -10,25 +10,42 @@ namespace CelestialTravels0_1
     {
         public void Load(GameContext gameContext)
         {
-            Console.WriteLine("Which game shall we load?");
-
-            string[] files = Directory.GetFiles(@"c:\CelestialTravels\Save", "*.Json");
-
-            foreach (string file in files)
+            while(true)
             {
-                Console.WriteLine(file);
+                try
+                {
+                    Console.WriteLine("Which game shall we load?");
+
+                    string[] files = Directory.GetFiles(@"c:\CelestialTravels\Save", "*.Json");
+
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine(file);
+                    }
+                    Console.WriteLine(@"Enter the name of the file you wish to load (do not include the c:\CelestialTravels\save\)");
+
+                    var gameToLoad = Console.ReadLine();
+
+                    // read file into a string and deserialize JSON to a type
+                    GameContext gameContextLoad = JsonConvert.DeserializeObject<GameContext>(File.ReadAllText(@"c:\CelestialTravels\Save\" + gameToLoad + ".json"));
+                    gameContext = gameContextLoad;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Something went wrong");
+                    Console.WriteLine(ex.ToString());
+                }
+                
+                gameContext.PlayerStats.PrintPlayerStats(gameContext);
+
+                gameContext.SpacePort.SpacePortOptions(gameContext);
+                break;
+
             }
-            Console.WriteLine(@"Enter the name of the file you wish to load (do not include the c:\CelestialTravels\save\)");
+           
+            
 
-            var gameToLoad = Console.ReadLine();
-
-            // read file into a string and deserialize JSON to a type
-            GameContext gameContextLoad = JsonConvert.DeserializeObject<GameContext>(File.ReadAllText(@"c:\CelestialTravels\Save\" + gameToLoad + ".json"));
-
-            gameContext = gameContextLoad;
-            gameContext.PlayerStats.PrintPlayerStats(gameContext);
-
-            gameContext.SpacePort.SpacePortOptions(gameContext);
+           
         }
 
         
--- TravelingExperiment/SaveGame.cs
+++ TravelingExperiment/SaveGame.cs
@@ -17,13 +17,25 @@ namespace CelestialTravels0_1
             {
                 Console.WriteLine(file);
             }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the name of your Save Game file.  Enter the same name to over write previous game.");
+                    Console.WriteLine(@"Do not include the (c:\CelestialTravels\Save\)");
+                    var saveGameName = Console.ReadLine();
 
-            Console.WriteLine("Please enter the name of your Save Game file.  Enter the same name to over write previous game.");
-            Console.WriteLine(@"Do not include the (c:\CelestialTravels\Save\)");
-            var saveGameName = Console.ReadLine();
-
-            // serialize JSON to a string and then write string to a file
-            File.WriteAllText(@"c:\CelestialTravels\Save\" + saveGameName + ".json", JsonConvert.SerializeObject(gameContext));
+                    // serialize JSON to a string and then write string to a file
+                    File.WriteAllText(@"c:\CelestialTravels\Save\" + saveGameName + ".json", JsonConvert.SerializeObject(gameContext));
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Something went wrong, try again");
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            
         }
     }
 }
