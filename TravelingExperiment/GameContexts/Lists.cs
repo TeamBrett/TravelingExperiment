937eb88d82d40a1567857c986d314d2586ba87a3
Jfleming <Jfleming07712@gmail.com>
2019-08-12 20:53:05 -0400

    PlayerMake refactored.  Time to test to see how bad I Fucked it up.

--- TravelingExperiment/GameContexts/Lists.cs
+++ TravelingExperiment/GameContexts/Lists.cs
@@ -8,11 +8,16 @@ namespace CelestialTravels0_1.GameContexts
 {
     public class Lists
     {
+        // Lists for traveling
         public List<JumpGate> listJumpGate { get; set; } = new List<JumpGate>();
         public List<SpacePort> listSpacePort { get; set; } = new List<SpacePort>();
         public List<Instance> listInstance { get; set; } = new List<Instance>();
-
+        // Lists for PlayerMaker
         public List<string> GenderList { get; set; } = new List<string> { "Male", "Female", "NonBinary" };
+        public List<string> RaceList { get; set; } = new List<string> { "Lizarian", "Cepholarian", "Fuzzarian" };
+        public List<string> JobList { get; set; } = new List<string> { "Navigator", "Gunslinger", "Timebender" };
+
+
 
     }
 }
--- TravelingExperiment/Players/PlayerMaker.cs
+++ TravelingExperiment/Players/PlayerMaker.cs
@@ -11,130 +11,104 @@ namespace CelestialTravels0_1.Players
     {
         public void CreatePlayer(GameContext gameContext)
         {
-            var genderValid = false;
-            var raceValid = false;
-            var jobValid = false;
-
-
-
             gameContext.Player.Name = GetPlayerName();
 
             
+            // Gender selection
+            var menuMax = MenuOptions.ListOptions(gameContext.List.GenderList);
 
-            do
+            Console.WriteLine($"\nWhat is your gender? ");
+
+            var enteredGender = Verify.UserInputForNumberedOptionMenu(menuMax);
+
+
+            if (enteredGender == 0)
             {
-                var menuMax = MenuOptions.ListOptions(gameContext.List.GenderList);
-
-                Console.WriteLine($"\nWhat is your gender? ");
-
-                var enteredGender = Verify.UserInputForNumberedOptionMenu(menuMax);
-
-
-                if (enteredGender == 0)
-                {
-                    gameContext.Player.Gender = "Male";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is {gameContext.Player.Gender}\n");
-                    genderValid = true;
-                }
-
-                else if (enteredGender == 1)
-                {
-                    gameContext.Player.Gender = "Female";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is {gameContext.Player.Gender}\n");
-                    genderValid = true;
-                }
-
-                else if (enteredGender == 2)
-                {
-                    gameContext.Player.Gender = "NonBinary";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is {gameContext.Player.Gender}\n");
-                    genderValid = true;
-                }
+                gameContext.Player.Gender = "Male";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is {gameContext.Player.Gender}\n");
             }
-            while (genderValid == false);
 
-
-            do
+            else if (enteredGender == 1)
             {
-                RaceOptions.ListRaces();
-
-                Console.WriteLine("\nWhat is your race?");
-
-                var enteredRace = Console.ReadLine();
-
-                if (enteredRace == "Lizarian")
-                {
-                    gameContext.Player.Race = "Lizarian";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race}\n");
-                    raceValid = true;
-                }
-
-                else if (enteredRace == "Cepholarian")
-                {
-                    gameContext.Player.Race = "Cepholarian";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race}\n");
-                    raceValid = true;
-                }
-
-                else if (enteredRace == "Fuzzarian")
-                {
-                    gameContext.Player.Race = "Fuzzarian";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race}\n");
-                    raceValid = true;
-                }
+                gameContext.Player.Gender = "Female";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is {gameContext.Player.Gender}\n");
             }
-            while (raceValid == false);
 
-            do
+            else if (enteredGender == 2)
             {
-                JobOptions.ListJobs();
-
-                Console.WriteLine("\nWhat is your job?");
-
-                var enteredJob = Console.ReadLine();
-
-                if (enteredJob == "Navigator")
-                {
-                    gameContext.Player.Job = "Navigator";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race} {gameContext.Player.Job}\n");
-                    gameContext.Player.NavigationSkill += 5;
-                    if (gameContext.Player.Race == "Lizarian")
-                    {
-                        Console.WriteLine($"{gameContext.Player.Name} the Navi >*^,^,^~~~");
-                    }
-                    Console.WriteLine("NavigationSkill +5\n");
-                    gameContext.PlayerStats.PrintPlayerStats(gameContext);
-                    Console.WriteLine("Return any key to continue...");
-                    Console.ReadLine();
-                    jobValid = true;
-                }
-
-                else if (enteredJob == "Gunslinger")
-                {
-                    gameContext.Player.Job = "Gunslinger";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race} {gameContext.Player.Job}\n");
-                    gameContext.Player.WeaponSkill += 5;
-                    Console.WriteLine("WeaponSkill +5\n");
-                    gameContext.PlayerStats.PrintPlayerStats(gameContext);
-                    Console.WriteLine("Return any key to continue...");
-                    Console.ReadLine();
-                    jobValid = true;
-
-                }
-
-                else if (enteredJob == "Timebender")
-                {
-                    gameContext.Player.Job = "Timebender";
-                    Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race} {gameContext.Player.Job}\n");
-                    gameContext.Player.TimeSkill += 5;
-                    Console.WriteLine("TimeSkill +5\n");
-                    gameContext.PlayerStats.PrintPlayerStats(gameContext);
-                    Console.WriteLine("Return any key to continue...");
-                    Console.ReadLine();
-                    jobValid = true;
-                }
+                gameContext.Player.Gender = "NonBinary";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is {gameContext.Player.Gender}\n");
             }
-            while (jobValid == false);
+
+            // Race Selection
+
+            Console.WriteLine("\nWhat is your race?");
+
+            var enteredRace = Verify.UserInputForNumberedOptionMenu(menuMax);
+
+            if (enteredRace == 0)
+            {
+                gameContext.Player.Race = "Lizarian";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race}\n");
+            }
+
+            else if (enteredRace == 1)
+            {
+                gameContext.Player.Race = "Cepholarian";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race}\n");
+            }
+
+            else if (enteredRace == 2)
+            {
+                gameContext.Player.Race = "Fuzzarian";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race}\n");
+            }
+
+            // Job Selection
+
+            menuMax = MenuOptions.ListOptions(gameContext.List.RaceList);
+
+            Console.WriteLine("\nWhat is your job?");
+
+            var enteredJob = Verify.UserInputForNumberedOptionMenu(menuMax);
+
+            if (enteredJob == 0)
+            {
+                gameContext.Player.Job = "Navigator";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race} {gameContext.Player.Job}\n");
+                gameContext.Player.NavigationSkill += 5;
+                if (gameContext.Player.Race == "Lizarian")
+                {
+                    Console.WriteLine($"{gameContext.Player.Name} the Navi >*^,^,^~~~");
+                }
+                Console.WriteLine("NavigationSkill +5\n");
+                gameContext.PlayerStats.PrintPlayerStats(gameContext);
+                Console.WriteLine("Return any key to continue...");
+                Console.ReadLine();
+            }
+
+            else if (enteredJob == 1)
+            {
+                gameContext.Player.Job = "Gunslinger";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race} {gameContext.Player.Job}\n");
+                gameContext.Player.WeaponSkill += 5;
+                Console.WriteLine("WeaponSkill +5\n");
+                gameContext.PlayerStats.PrintPlayerStats(gameContext);
+                Console.WriteLine("Return any key to continue...");
+                Console.ReadLine();
+
+            }
+
+            else if (enteredJob == 2)
+            {
+                gameContext.Player.Job = "Timebender";
+                Console.WriteLine($"\n{ gameContext.Player.Name} is a {gameContext.Player.Gender} {gameContext.Player.Race} {gameContext.Player.Job}\n");
+                gameContext.Player.TimeSkill += 5;
+                Console.WriteLine("TimeSkill +5\n");
+                gameContext.PlayerStats.PrintPlayerStats(gameContext);
+                Console.WriteLine("Return any key to continue...");
+                Console.ReadLine();
+            }
         }
 
         public string GetPlayerName()
@@ -156,8 +130,6 @@ namespace CelestialTravels0_1.Players
                     Console.WriteLine("You must enter a valid name");
                 }
             }
-            
-
 
             return name;
         }
