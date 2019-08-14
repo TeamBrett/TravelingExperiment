eedaf30406f87cfe755a1bdacac032bed3afcaf6
Jfleming <Jfleming07712@gmail.com>
2019-08-13 20:38:09 -0400

    Working on making spells, and refactoring Fight method as I go along.

---
+++ TravelingExperiment/Bases/Spell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CelestialTravels0_1.Bases
+{
+    public class Spell
+    {
+        public double ManaConsumed { get; set; }
+        public string Name { get; set; }
+        public string UsedBy { get; set; }
+
+    }
+}
---
+++ TravelingExperiment/Spells/VirtualGun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CelestialTravels0_1.Bases;
+using CelestialTravels0_1.Consumables;
+using CelestialTravels0_1.Consumables.EnergyKits;
+using CelestialTravels0_1.Consumables.HealthKits;
+using CelestialTravels0_1.Fights;
+using CelestialTravels0_1.GameContexts;
+using CelestialTravels0_1.Monsters;
+using CelestialTravels0_1.Places;
+using CelestialTravels0_1.Players;
+using CelestialTravels0_1.Save_Load;
+using CelestialTravels0_1.Travels;
+using CelestialTravels0_1.Verifications;
+using CelestialTravels0_1.Weapons;
+
+
+namespace CelestialTravels0_1.Spells
+{
+    public class VirtualGun : Spell
+    {
+        public void Use(GameContext gameContext)
+        {
+            Console.WriteLine("How much Energy would you like to use?");
+            Console.WriteLine($"You have {gameContext.Player.EnergyCurrent}/{gameContext.Player.EnergyTotal} energy");
+
+            var tempUserInput = Console.ReadLine();
+
+            var playerSelection = Verify.UserInputForNumberedOptionMenu(tempUserInput, (int)gameContext.Player.EnergyCurrent);
+
+            gameContext.Player.EnergyTotal -= playerSelection;
+
+            var damage = playerSelection;
+        }
+    }
+}
