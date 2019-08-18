2bd7a8c15c6bd5f73c9ff8815e98a1d7f734cd35
Brett <teambrett9000@gmail.com>
2019-08-17 18:48:27 -0700

    database

---
+++ DataGetter/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace DataGetter
+{
+    class Program
+    {
+        private static string dbConnectionString = "Server=localhost;Database=Test;Trusted_Connection=True;MultipleActiveResultSets=true";
+        private static SqlConnection connection;
+
+        static void Main(string[] args) {
+            using (connection = new SqlConnection(dbConnectionString)) {
+                Console.WriteLine("Which player would you like?");
+
+                var id = int.Parse(Console.ReadLine());
+                var player = GetPlayerTheEasierWay(id);
+                if (player == null) {
+                    Console.WriteLine("nope, can't find that player");
+                    return;
+                }
+
+                Console.WriteLine(player.Name + " is a " + player.Gender.Name);
+
+                Console.WriteLine("New player name: ");
+
+                var player2 = new Player();
+                player2.Name = Console.ReadLine();
+
+                Console.WriteLine("new Player gender: ");
+                player2.GenderId = short.Parse(Console.ReadLine());
+                AddPlayer(player2);
+
+                Console.WriteLine("player 2 id: " + player2.Id);
+                Console.ReadLine();
+
+
+                var battleResults = new BattleResults();
+
+                this.battleResultsDao.Insert(battleResults);
+            }
+        }
+
+        private static void AddPlayer(Player player) {
+            var id = connection.QuerySingle<int>($@"
+
+insert into {nameof(Player)}(name, genderid)
+values(@playername, @genderid)
+
+select scope_identity();
+
+",
+new {
+    playername = player.Name,
+    player.GenderId
+});
+
+            player.Id = id;
+        }
+
+        private static Player GetPlayerTheEasierWay(int id) {
+            return connection.Query<Player, Option, Player>($@"
+SELECT 
+  p.id            as {nameof(Player.Id)}
+, p.name          as {nameof(Player.Name)}
+, p.genderid      as {nameof(Player.GenderId)}
+, g.id
+, g.name
+, g.displayindex
+FROM dbo.Player p
+join dbo.gender g on p.GenderId = g.Id
+WHERE p.id = @id
+",
+                (player, option) => {
+                    player.Gender = option;
+                    return player;
+                }, new { id }).First();
+        }
+
+        private static Player GetPlayerTheHardWay(int id) {
+            using (var sqlConnection1 = new SqlConnection("Server=localhost;Database=Test;Trusted_Connection=True;MultipleActiveResultSets=true")) {
+                using (var cmd = new SqlCommand() {
+                    CommandText = @"
+SELECT 
+    id
+  , name as PlayerName
+  , genderid
+FROM dbo.Player
+WHERE id = @id
+",
+                    CommandType = CommandType.Text,
+                    Connection = sqlConnection1
+                }) {
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                    sqlConnection1.Open();
+
+                    using (var reader = cmd.ExecuteReader()) {
+                        if (reader.Read()) {
+                            var player = new Player();
+                            player.Id = (int)reader[0];
+                            player.Name = (string)reader[1];
+                            player.GenderId = (short)reader[2];
+
+                            return player;
+                        }
+
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+
+    class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short GenderId { get; set; }
+        public Option Gender { get; set; }
+    }
+
+    class Option
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayIndex { get; set; }
+    }
+}
---
+++ Database1/Script1.sql
@@ -0,0 +1,75 @@
+﻿
+select *
+from Player
+order by name
+
+insert into Player ([Name], GenderId)
+values('Slagathor', 1)
+
+update Player set Name = 'Padwin' where Id = 3
+
+delete player where id = 2
+
+insert into Player (Name, GenderId)
+values('Max', 1)
+
+
+create table Gender(
+	Id smallint not null Primary key,
+	Name nchar(25) not null,
+	DisplayIndex int not null
+)
+
+drop table Gender
+
+insert into Gender (id, name, DisplayIndex) values
+  (1, 'Male', 1)
+, (2, 'Female', 2)
+, (3, 'Non Binary', 3)
+
+alter table player add constraint FK__player__gender foreign key (GenderId) references Gender(Id)
+
+
+select p.id, p.name as PlayerName, g.name as Gender
+from player p
+join gender g on p.GenderId = g.Id
+order by p.Name
+
+update player set genderid = 2 where id = 3
+
+-- dudes or chicks?
+select count(*)
+from player p
+join gender g on p.GenderId = g.Id
+where g.name = 'Male'
+
+
+select g.name, count(*)
+from player p
+join gender g on p.GenderId = g.Id
+group by g.name
+
+
+from player p
+where p.genderid = 3
+select p.name
+
+
+-- change column definition
+alter table gender alter column name nvarchar(25) not null
+
+-- rename column
+EXEC sp_rename 'Test.dbo.Gender', 'GenderId', 'COLUMN';
+
+begin transaction
+	insert into gender(id, name, displayindex)values(10, 'alien', 500)
+
+	insert into Player ([Name], GenderId)
+	values('Beau', 10)
+
+	select * from player
+
+--rollback
+commit
+
+select * from player
